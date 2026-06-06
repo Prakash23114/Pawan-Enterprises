d72a005720f26c79148c495b08aa0f294fe8cd9a
Prakash23114 <prakashmandal23114@gmail.com>
2026-06-06 19:12:52 +0530

    Fix nodemailer config

--- backend/config/email.js
+++ backend/config/email.js
@@ -4,9 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: parseInt(process.env.EMAIL_PORT || '465'),
-  secure: true, // true for port 465 (SSL)
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
